9c27d4a104ceda32c16210caea3fc39c7568e3e2
Fernando José González Bernáldez <ferferjose99@gmail.com>
2025-04-26 18:58:28 +0200

    Added login mockup

--- resources/views/mockups/mockup.blade.php
+++ resources/views/mockups/mockup.blade.php
@@ -2,63 +2,62 @@
 <html lang="es">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Fondo Diagonal con Formulario</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
   <link rel="stylesheet" href="{{ asset('css/bootstrap/bootstrap.min.css') }}">
   <link rel="stylesheet" href="{{ asset('css/common.css') }}">
   <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 </head>
-<body class="fondo">
-    
-    <div class="corte-purpura"></div>
-    <div class="linea-blanca"></div>
-    <div class="d-flex flex-row justify-content-center align-items-center">
-    </div>
-
-    <div class="left-content d-flex flex-column align-items-center justify-content-center text-start ps-6rem">
-        <h1 class="w-100 fs-title fw-bold">Take control of your finances</h1>
-        <p class="w-100 fs-2">
-            Start saving, organize your income and expenses, and reach your goals with Rivo.
-        </p>
-    </div>
-
-    <div class="w-100 d-flex flex-column aling-items-center justify-content-center">
-        <div class="login-container mx-auto">
-            <h2 class="fs-1">Iniciar sesión</h2>
-            <form action="">
-                <label for="email">Correo electrónico</label>
-                <div class="input-group">
-                    <i class="fas fa-user"></i>
-                    <input type="email" id="email" placeholder="Correo electrónico"/>
-                </div>
-                <label for="password">Contraseña</label>
-                <div class="input-group">
-                    <i class="fas fa-lock"></i>
-                    <input type="password" id="password" placeholder="Contraseña" />
-                    <i class="fas fa-eye"></i>
-                    <i class="fas fa-eye-slash d-none"></i>
-
-                </div>
-
-                <div class="options">
-                    <label><input type="checkbox" /> Remember Me</label>
-                    <a href="#">¿Has olvidado tu contraseña?</a>
-                </div>
-
-                <div class="register">
-                    <p>¿No registrado? <a href="#">Regístrate aquí</a></p>
-                </div>
-
-                <button class="login-btn w-100">Iniciar sesion</button>
-                <hr/>
-                <button class="google-btn w-100">
-                    <img class="google-logo" src="{{ asset('img/googleLogo.png') }}" alt="">Login with google
-                </button>
-            </form>
+<body>
+    <div class="container min-vh-100 d-flex align-items-center justify-content-center">
+        <div class="row justify-content-center align-items-center py-3 ">
+            <div class="col-12 col-lg-6 row text-white">
+                <h1 class="w-100 fs-1 fw-bold text-center text-lg-start">Bienvenido a Rivo</h1>
+                <p class="w-100 fs-4 subtitle-mobile">
+                    Start saving, organize your income and expenses, and reach your goals with Rivo.
+                </p>
+            </div>
         
+            <div class="row col-12 col-lg-6 mt-4 mt-lg-0">
+                <div class="login-container mx-auto">
+                    <h2 class="fw-bold fs-2">Iniciar sesión</h2>
+                    <form action="">
+                        <label for="email" class="form-label">Correo electrónico</label>
+                        <div class="input-group mb-4">
+                            <i class="fas fa-user"></i>
+                            <input type="email" id="email" placeholder="Correo electrónico"/>
+                        </div>
+                        <label for="password">Contraseña</label>
+                        <div class="input-group mb-2">
+                            <i class="fas fa-lock"></i>
+                            <input type="password" id="password" placeholder="Contraseña" />
+                            <i class="fas fa-eye"></i>
+                            <i class="fas fa-eye-slash d-none"></i>
+                        </div>
+                        
+                        <div class="d-flex flex-column flex-lg-row justify-content-start justify-content-lg-between align-items-start align-items-lg-center w-100 p-1 mb-3 text-start">
+                            <div class="d-flex align-items-center gap-1">
+                                <input type="checkbox" id="rememberMe" />
+                                <label for="rememberMe" class="fs-6 m-0">Recordarme</label>
+                            </div>
+                            <a href="#" class="fs-6 link-login">¿Has olvidado tu contraseña?</a>
+                            </div>
+        
+                        <div class="register mb-2">
+                            <p  class="fs-6">¿No registrado? <a href="#">Regístrate aquí</a></p>
+                        </div>
+        
+                        <button class="login-btn w-100 fs-4 fw-bold">Iniciar sesión</button>
+                        <hr class="my-4" />
+                        <button class="google-btn w-100 fs-4 fw-bold">
+                            <img class="google-logo" src="{{ asset('img/googleLogo.png') }}" alt="Google logo">
+                            <span>Login with Google</span>
+                        </button>
+                    </form>
+                </div>
+            </div>
         </div>
-
     </div>
-
 </body>
 </html>
