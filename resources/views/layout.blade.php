85a3508fe025de3dd566ed050f18734465051b71
Angel-Guiberteau <aguiberteaufranco.guadalupe@alumnado.fundacionloyola.net>
2025-05-06 00:04:32 +0200

    Añadido layout, header, JS and CSS component, favIco y Stacks CSS y JS

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        @include('header');
+    </head>
+    <body>
+        
+        @yield('content')
+
+        {{-- @include('footer') --}}
+    </body>
+</html>
---
+++ resources/views/projectJS.blade.php
@@ -0,0 +1,14 @@
+@section('projectJS')
+    
+    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.js"></script>
+
+    <script src="{{ asset('js/bootstrap-datepicker/bootstrap-datepicker.min.js') }}"></script>
+    <script src="{{ asset('js/datepicker/bootstrap-datepicker.en-US.min.js') }}"></script>
+    <script src="{{ asset('js/datepicker/bootstrap-datepicker.es.min.js') }}"></script>
+    <script src="{{ asset('js/datepicker/bootstrap-datepicker.fr.min.js') }}"></script>
+
+    <script src="{{ asset('js/sweetalert/sweetalert.min.js') }}"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+@endsection
