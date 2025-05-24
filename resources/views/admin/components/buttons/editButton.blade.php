f900836926d80d2f7122aeae5c6b0d8fc69d19a7
Angel-Guiberteau <aguiberteaufranco.guadalupe@alumnado.fundacionloyola.net>
2025-05-24 10:28:23 +0200

    fixedListUsersView

--- resources/views/admin/components/buttons/editButton.blade.php
+++ resources/views/admin/components/buttons/editButton.blade.php
@@ -1,3 +1,3 @@
-<button class="btn btn-edit btn-sm" {!! $data ?? ' ' !!}>
+<button class="btn btn-edit btn-sm" {!! $data ?? '' !!} {!! isset($onclick) ? 'onclick="'.$onclick.'"' : '' !!}>
     <i class="fa-solid fa-pen-to-square"></i>
 </button>
