497850d34820068edeb372656d75d6152539482a
Angel-Guiberteau <aguiberteaufranco.guadalupe@alumnado.fundacionloyola.net>
2025-06-03 20:53:19 +0200

    errorMsgSpain

---
+++ resources/lang/es/fortify.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'two_factor_authentication' => 'Autenticación en dos pasos',
+    'confirm_password' => 'Confirma tu contraseña antes de continuar.',
+    'failed' => 'Estas credenciales no coinciden con nuestros registros.',
+    'password' => 'La contraseña proporcionada es incorrecta.',
+    'throttle' => 'Demasiados intentos. Por favor intenta de nuevo en :seconds segundos.',
+];
